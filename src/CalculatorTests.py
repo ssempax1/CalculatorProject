0746833a3de9677b4f02e4ddca1b3ecf631709b8
ian <isk5@njit.edu>
2020-06-24 23:15:30 -0400

    Feature: Csv reader works as expected

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -17,45 +17,9 @@ class MyTestCase(unittest.TestCase):
 
     def test_subtract_method_calculator(self):
         test_data = CsvReader('src/Subtraction.csv').data
-        pprint(test_data)
         for row in test_data:
-            self.assertEqual(self.calculator.subtract(row['value 1'], row['value 2']), int(row['result']))
-            self.assertEqual(self.calculator.result, int(row['result']))
-
-    def test_add_method_calculator(self):
-        test_data = CsvReader('src/Addition.csv').data
-        pprint(test_data)
-        for row in test_data:
-            self.assertEqual(self.calculator.add(row['value 1'], row['value 2']), row['Result'])
-            self.assertEqual(self.calculator.result, row['Result'])
-
-    def test_multiply_method_calculator(self):
-        test_data = CsvReader('src/Multiplication.csv').data
-        pprint(test_data)
-        for row in test_data:
-            self.assertEqual(self.calculator.multiply(row['value 1'], row['value 2']), row['Result'])
-            self.assertEqual(self.calculator.result, row['Result'])
-
-    def test_divide_method_calculator(self):
-        test_data = CsvReader('src/Division.csv').data
-        pprint(test_data)
-        for row in test_data:
-            self.assertEqual(self.calculator.divide(row['value 1'], row['value 2']), row['Result'])
-            self.assertEqual(self.calculator.result, row['Result'])
-
-    def test_sqr_method_calculator(self):
-        test_data = CsvReader('src/Square.csv').data
-        pprint(test_data)
-        for row in test_data:
-            self.assertEqual(self.calculator.sqr(row['value 1']), row['Result'])
-            self.assertEqual(self.calculator.result, row['Result'])
-
-    def test_sqrt_method_calculator(self):
-        test_data = CsvReader('src/Square_Root.csv').data
-        pprint(test_data)
-        for row in test_data:
-            self.assertEqual(self.calculator.sqrt(row['value 1']), row['Result'])
-            self.assertEqual(self.calculator.result, row['Result'])
+            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
 
 if __name__ == '__main__':
